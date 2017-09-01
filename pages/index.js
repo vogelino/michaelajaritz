dd444d1423ce4778444b2db54329c925731bb827
vogelino <contact@vogelino.com>
2017-09-01 22:20:34 +0200

    Added Next.JS boilderplate with one page and HtmlHeader

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HtmlHead from '../components/HtmlHead.js';
+
+export default () => (
+	<div>
+		<HtmlHead pageTitle="Willkommen" />
+		<div>Welcome to next.js!</div>
+	</div>
+);
