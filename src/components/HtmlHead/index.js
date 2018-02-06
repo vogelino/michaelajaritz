d67ca2a1418e9dcf20c0d18f5337b4a8fb70cf65
vogelino <contact@vogelino.com>
2018-02-06 20:54:19 +0100

    Add favicons

--- src/components/HtmlHead/index.js
+++ src/components/HtmlHead/index.js
@@ -15,6 +15,16 @@ export const HtmlHeadComponent = ({ pageName, intl, children }) => (
 		<meta name="distribution" content="web" />
 		<meta name="robots" content="index, follow" />
 		<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+
+		<link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png?v=47MJ98MQ8B" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png?v=47MJ98MQ8B" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png?v=47MJ98MQ8B" />
+		<link rel="manifest" href="/static/favicons/site.webmanifest?v=47MJ98MQ8B" />
+		<link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg?v=47MJ98MQ8B" color="#5bbad5" />
+		<link rel="shortcut icon" href="/static/favicons/favicon.ico?v=47MJ98MQ8B" />
+		<meta name="msapplication-TileColor" content="#ca008c" />
+		<meta name="msapplication-config" content="/static/favicons/browserconfig.xml?v=47MJ98MQ8B" />
+		<meta name="theme-color" content="#ffffff" />
 		{children}
 	</Head>
 );
