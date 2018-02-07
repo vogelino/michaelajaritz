c49fe3bddef9770c07e0577f052dd3c479bda85f
vogelino <contact@vogelino.com>
2018-02-07 21:14:51 +0100

    Update the favicons for IE

--- src/components/HtmlHead/index.js
+++ src/components/HtmlHead/index.js
@@ -16,14 +16,17 @@ export const HtmlHeadComponent = ({ pageName, intl, children }) => (
 		<meta name="robots" content="index, follow" />
 		<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
 
-		<link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png?v=47MJ98MQ8B" />
-		<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png?v=47MJ98MQ8B" />
-		<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png?v=47MJ98MQ8B" />
-		<link rel="manifest" href="/static/favicons/site.webmanifest?v=47MJ98MQ8B" />
-		<link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg?v=47MJ98MQ8B" color="#5bbad5" />
-		<link rel="shortcut icon" href="/static/favicons/favicon.ico?v=47MJ98MQ8B" />
-		<meta name="msapplication-TileColor" content="#ca008c" />
-		<meta name="msapplication-config" content="/static/favicons/browserconfig.xml?v=47MJ98MQ8B" />
+		<link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png?v=RyMgLlLMO9" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png?v=RyMgLlLMO9" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png?v=RyMgLlLMO9" />
+		<link rel="manifest" href="/static/favicons/site.webmanifest?v=RyMgLlLMO9" />
+		<link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg?v=RyMgLlLMO9" color="#5bbad5" />
+		<link rel="shortcut icon" type="image/x-icon" href="/static/favicons/favicon.ico?v=RyMgLlLMO9" />
+		<meta name="apple-mobile-web-app-title" content="Michaela Jaritz" />
+		<meta name="application-name" content="Michaela Jaritz" />
+		<meta name="msapplication-TileColor" content="#ff0000" />
+		<meta name="msapplication-TileImage" content="/static/favicons/mstile-144x144.png?v=RyMgLlLMO9" />
+		<meta name="msapplication-config" content="/static/favicons/browserconfig.xml?v=RyMgLlLMO9" />
 		<meta name="theme-color" content="#ffffff" />
 		{children}
 	</Head>
