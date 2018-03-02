6df6083cdfe5bcb5433fcb7fda037e131786358d
vogelino <contact@vogelino.com>
2018-03-02 21:00:46 +0100

    Remove tests

--- src/texts/de-DE.test.js
+++
@@ -1,7 +0,0 @@
-/* global expect, test */
-import germanTexts from './de-DE';
-
-test('The text file should export an object', () => {
-	expect(typeof germanTexts).toBe('object');
-	expect(Object.keys(germanTexts).length).toBeGreaterThan(0);
-});
--- src/utils/optimizedResize/index.test.js
+++
@@ -1,6 +0,0 @@
-import getOptimizedResize from '.';
-
-test('It should return an object with the add method', () => {
-	const optimizedResize = getOptimizedResize();
-	expect(typeof optimizedResize.add).toBe('function');
-});
