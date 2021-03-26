d9aa9635fae529ffa53177e914342e4856a92116
Lucas Vogel <contact@vogelino.com>
2021-03-26 19:52:47 +0100

    fix(idee): Fix broken idee page

--- pages/idee.js
+++ pages/idee.js
@@ -12,32 +12,32 @@ const IdeaPage = () => (
 			src="/images/portraits/mobile/welcomeMain.jpg"
 			alt="Training, Coaching, Moderation."
 		/>
-		<Subtitle timeout={1000}>{getText('subtitle')}</Subtitle>
+		<Subtitle timeout={1000}>{getText(`pages.idee.content.subtitle`)}</Subtitle>
 		<BigTitle timeout={1200} color="orange" marginBottom={10}>
-			{getText('titleTraining')}
+			{getText(`pages.idee.content.titleTraining`)}
 		</BigTitle>
 		<BigTitle timeout={1400} color="blue" marginBottom={10} marginTop={0}>
-			{getText('titleCoaching')}
+			{getText(`pages.idee.content.titleCoaching`)}
 		</BigTitle>
 		<BigTitle timeout={1600} color="purple" marginTop={0}>
-			{getText('titleModeration')}
+			{getText(`pages.idee.content.titleModeration`)}
 		</BigTitle>
-		<Paragraph timeout={1800}>{getText('textIntro')}</Paragraph>
+		<Paragraph timeout={1800}>{getText(`pages.idee.content.textIntro`)}</Paragraph>
 		<MobilePicture src="/images/artworks/mobile/experienced.jpg" alt="Sichtbar machen." />
 		<BigTitle timeout={1900} color="purple">
-			{getText('titleMakeVisible')}
+			{getText(`pages.idee.content.titleMakeVisible`)}
 		</BigTitle>
-		<Paragraph timeout={2000}>{getText('textMakeVisible')}</Paragraph>
+		<Paragraph timeout={2000}>{getText(`pages.idee.content.textMakeVisible`)}</Paragraph>
 		<MobilePicture src="/images/artworks/mobile/makespace.jpg" alt="Platz machen." />
 		<BigTitle timeout={2100} color="orange">
-			{getText('titleMakeRoom')}
+			{getText(`pages.idee.content.titleMakeRoom`)}
 		</BigTitle>
-		<Paragraph timeout={2200}>{getText('textMakeRoom')}</Paragraph>
+		<Paragraph timeout={2200}>{getText(`pages.idee.content.textMakeRoom`)}</Paragraph>
 		<MobilePicture src="/images/artworks/mobile/possible.jpg" alt="Möglich machen." />
 		<BigTitle timeout={2300} color="blue">
-			{getText('titleMakePossible')}
+			{getText(`pages.idee.content.titleMakePossible`)}
 		</BigTitle>
-		<Paragraph timeout={2400}>{getText('textMakePossible')}</Paragraph>
+		<Paragraph timeout={2400}>{getText(`pages.idee.content.textMakePossible`)}</Paragraph>
 	</Page>
 )
 
